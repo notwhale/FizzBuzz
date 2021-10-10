8e647bf831ef52278e2d78965414a1a8660b314f
Nikita Kapishev <notwhale@gmail.com>
2021-10-10 15:11:04 +0500

    Modify FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,15 @@
 #!/usr/env python3
 
 """
-Задача fizz_buzz:
-    Напиши программу, которая выводит на экран числа от 1 до 100.
-    При этом вместо чисел, кратных трем, программа должна выводить слово Fizz,
-        а вместо чисел, кратных пяти — слово Buzz.
-    Если число кратно пятнадцати, то программа должна выводить слово FizzBuzz.
-    Решение должно быть простым и понятным.
+Write a program to print numbers from 1 to 100.
+But for multiples of 3 print 'Fizz' instead and
+    for the multiple of 5 print 'Buzz' and
+    for the numbers multiple of both 3 and 5 print 'FizzBuzz'.
 """
 
 import sys
 import time
+
 
 def fizz_buzz1(r_min, r_max):
     """
@@ -28,15 +27,17 @@ def fizz_buzz1(r_min, r_max):
             result.append(num)
     return result
 
+
 def fizz_buzz2(r_min, r_max):
     """
     With ternary condition
     """
     return list(
-        ('' if num % 3 else 'Fizz') + 
+        ('' if num % 3 else 'Fizz') +
         ('' if num % 5 else 'Buzz') or
         num for num in range(r_min, r_max + 1)
     )
+
 
 def fizz_buzz3(r_min, r_max):
     """
@@ -44,12 +45,13 @@ def fizz_buzz3(r_min, r_max):
     """
     return list(
         map(
-            lambda num: 
-                ('Fizz' if num % 3 == 0 else '') + 
-                ('Buzz' if num % 5 == 0 else '') or 
+            lambda num:
+                ('Fizz' if num % 3 == 0 else '') +
+                ('Buzz' if num % 5 == 0 else '') or
                 num, range(r_min, r_max + 1)
         )
     )
+
 
 def main(r_min=1, r_max=101):
     """
@@ -62,9 +64,10 @@ def main(r_min=1, r_max=101):
     )
     for f in fizz_buzz_all:
         start = time.time()
-        print(f)
+        print(*f, sep='\n')
         end = time.time()
         print("Execution time: ", end - start)
+
 
 if __name__ == "__main__":
     sys.exit(main())
